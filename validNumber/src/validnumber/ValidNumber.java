1a1a12143ca22a0848b72f398d82ea6e1c61bd27
Martin Cimbalek <mcimbale@redhat.com>
2016-08-16 15:12:32 +0200

    falled back again and made transition table static - still slow
    Runtime: 4 ms
    Beaten: 61.35% solutions

--- validNumber/src/validnumber/ValidNumber.java
+++ validNumber/src/validnumber/ValidNumber.java
@@ -11,6 +11,13 @@ package validnumber;
  */
 public class ValidNumber {
 
+    private static short[][] transitionTable = {
+        {0, 0, 8, 0, 8, 8, 0, 8, 8, 9, 0}, //white
+        {0, 2, 2, 5, 5, 5, 7, 7, 0, 2, 7}, //digit
+        {0, 3, 4, 0, 0, 0, 0, 0, 0, 3, 0}, //'.'
+        {0, 0, 6, 0, 6, 6, 0, 0, 0, 0, 0}, //'e'    
+        {0, 0, 0, 0, 0, 0, 10, 0, 0, 1, 0}};   //-
+
     /**
      * @param args the command line arguments
      */
@@ -19,45 +26,50 @@ public class ValidNumber {
     }
 
     public boolean isNumber(String s) {
-        short[][] transitionTable = {
-            {0, 0, 8, 0, 8, 8, 0, 8, 8, 9, 0}, //white
-            {0, 2, 2, 5, 5, 5, 7, 7, 0, 2, 7}, //digit
-            {0, 3, 4, 0, 0, 0, 0, 0, 0, 3, 0}, //'.'
-            {0, 0, 6, 0, 6, 6, 0, 0, 0, 0, 0}, //'e'    
-            {0, 0, 0, 0, 0, 0, 10, 0, 0, 1, 0}};   //-
 
         short status = 9;
         for (char c : s.toCharArray()) {
-            if (c >= '0' && c <= '9') {
-                status = transitionTable[1][status];
-            } else {
-                switch (c) {
-                    case ' ':
-                    case '\t': {
-                        status = transitionTable[0][status];
-                        break;
-                    }
-                    case '.': {
-                        status = transitionTable[2][status];
-                        break;
-                    }
-                    case 'e': {
-                        status = transitionTable[3][status];
-                        break;
-                    }
-                    case '-':
-                    case '+': {
-                        status = transitionTable[4][status];
-                        break;
-                    }
-                    default:
-                        return false;
+
+            switch (c) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9': {
+                    status = transitionTable[1][status];
+                    break;
                 }
-                if (status == 0) {
+                case ' ':
+                case '\t': {
+                    status = transitionTable[0][status];
+                    break;
+                }
+                case '.': {
+                    status = transitionTable[2][status];
+                    break;
+                }
+                case 'e': {
+                    status = transitionTable[3][status];
+                    break;
+                }
+                case '-':
+                case '+': {
+                    status = transitionTable[4][status];
+                    break;
+                }
+                default:
                     return false;
-                }
+            }
+            if (status == 0) {
+                return false;
             }
         }
+
         if (status == 2
                 || status == 4
                 || status == 5
